9491cf8b38c46c4471593f905cc3a98df659d134
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-02-25 00:33:40 +0100

    fix(test): fixed DateTools tests because it is not working on a computer with a different timezone than the one it was originaly coded with

--- src/test/java/ca/codepit/tw2md/DateToolsTest.java
+++ src/test/java/ca/codepit/tw2md/DateToolsTest.java
@@ -3,6 +3,8 @@ package ca.codepit.tw2md;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,10 +16,17 @@ class DateToolsTest {
 
 	@Test
 	public void canParseATiddlyWikiTimestamp() {
+		// Timestamp is always UTC, see https://en.wikipedia.org/wiki/Unix_time
+		//
+		// We explicitely convert first to ZonedDateTime UTC to avoid broken test 
+		// thanks to timezone differences between devs computers around the world.
+		final String tiddlyWikiTimestampInUTC = "20210920015946441";
+		final ZonedDateTime expectedUTCZonedDateTime = ZonedDateTime.of(LocalDateTime.of(2021, 9, 20, 01, 59, 46, 441 * 1000000), ZoneId.of("UTC"));
 
-		final LocalDateTime expected = LocalDateTime.of(2021, 9, 19, 21, 59, 46, 441 * 1000000);
-		final Optional<LocalDateTime> ts = DateTools.ts("20210920015946441");
-		assertEquals(Optional.of(expected), ts);
+
+		final LocalDateTime expectedLocalDateTimeOnSystemTimeZone = expectedUTCZonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime();
+		final Optional<LocalDateTime> parsedTiddlyWikiTimestampAsLocalDateTimeOnSystemTimeZone = DateTools.parseTiddlyWikiTimestampAsLocalDateTime(tiddlyWikiTimestampInUTC);
+		assertEquals(Optional.of(expectedLocalDateTimeOnSystemTimeZone), parsedTiddlyWikiTimestampAsLocalDateTimeOnSystemTimeZone);
 	}
 
 }
